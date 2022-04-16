0717e1a4e1ce2fce1e32071f7b08caf47a96967e
Sean <Sheath51@gmail.com>
2022-04-15 18:11:49 -0600

    Updating testbench.
    Testbench passes for various direct mapped cache sizes. Fails for 2-way set associative. Probably an error in the actual cache, not the testbenh.

--- sim/core_tb/neorv32_dcache_memory_tb.vhd
+++ sim/core_tb/neorv32_dcache_memory_tb.vhd
@@ -42,6 +42,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 library neorv32;
 use neorv32.neorv32_package.all;
@@ -52,7 +53,7 @@ entity tb_neorv32_dcache_memory is
     CLOCK_FREQUENCY   : natural := 50000000; -- clock frequency of clk_i in Hz
     DCACHE_NUM_BLOCKS : natural := 8;        -- number of blocks (min 1), has to be a power of 2
     DCACHE_BLOCK_SIZE : natural := 64;       -- block size in bytes (min 4), has to be a power of 2
-    DCACHE_NUM_SETS   : natural := 1;        -- associativity; 1=direct-mapped, 2=2-way set-associative
+    DCACHE_NUM_SETS   : natural := 2;        -- associativity; 1=direct-mapped, 2=2-way set-associative
     SELF_TERM         : boolean := true      -- When true the testbench will stop running if pass/fail has been determined
   );
 end tb_neorv32_dcache_memory;
@@ -60,53 +61,56 @@ end tb_neorv32_dcache_memory;
 architecture tb_neorv32_dcache_memory_rtl of tb_neorv32_dcache_memory is
 
   -- cache layout --
-  constant cache_entries_c  : integer    := DCACHE_NUM_BLOCKS * (DCACHE_BLOCK_SIZE/4); -- number of 32-bit entries (per set)
+  constant CACHE_OFFSET_SIZE_C : natural                                       := index_size_f(DCACHE_BLOCK_SIZE/4);                         -- offset addresses full 32-bit words
+  constant CACHE_INDEX_SIZE_C  : natural                                       := index_size_f(DCACHE_NUM_BLOCKS);
+  constant CACHE_ENTRIES_C     : integer                                       := DCACHE_NUM_BLOCKS * (DCACHE_BLOCK_SIZE/4);                 -- number of 32-bit entries (per set)
+  constant CACHE_TAB_INC       : integer                                       := 2**(CACHE_OFFSET_SIZE_C + CACHE_INDEX_SIZE_C + 2 - 1) - 1; -- add two for byte offset
+  constant CACHE_OFFSET_INC    : integer                                       := 2**(CACHE_OFFSET_SIZE_C - 1) - 1;
+  constant CACHE_INDEX_INC     : integer                                       := 2**(CACHE_INDEX_SIZE_C - 1) - 1;
 
   -- internals - hands off! --
-  constant t_clock_c        : time       := (1 sec) / CLOCK_FREQUENCY;
-  constant SELF_TERM_TIME   : time       := 1 ms;
-  constant MAX_CYCLES       : natural    := SELF_TERM_TIME / t_clock_c;
+  constant T_CLOCK_C           : time                                          := (1 sec) / CLOCK_FREQUENCY;
+  constant SELF_TERM_TIME      : time                                          := 1 ms;
+  constant MAX_CYCLES          : natural                                       := SELF_TERM_TIME / T_CLOCK_C;
 
   -- generators --
-  signal run_tb             : std_ulogic := '1';
-  signal clk_gen            : std_ulogic := '0';
-  signal rst_gen            : std_ulogic := '0';
+  signal run_tb                : std_ulogic                                    := '1';
+  signal clk_gen               : std_ulogic                                    := '0';
+  signal rst_gen               : std_ulogic                                    := '0';
 
   -- testbench signals
-  signal curr_addr          : unsigned(31 downto 0)         := (others => '0');
-  signal curr_set           : unsigned(31 downto 0)         := (others => '0');
-  signal init_mem           : std_ulogic := '1';
-  signal mem_busy           : std_ulogic := '0';
-  signal wrt_success        : std_ulogic := '0';
-  signal ctrl_en            : std_ulogic := '0';
-  signal ctrl_we            : std_ulogic := '0';
-  signal ctrl_tag_we        : std_ulogic := '0';
-  signal ctrl_valid_we      : std_ulogic := '0';
-  signal ctrl_invalid_we    : std_ulogic := '0';
-  signal ctrl_addr          : std_ulogic_vector(31 downto 0) := x"00000000";
-  signal ctrl_wdata         : std_ulogic_vector(31 downto 0) := x"00000000";
+  signal curr_addr             : unsigned(31 downto 0)                         := (others => '0');
+  signal curr_set              : unsigned(31 downto 0)                         := (others => '0');
+  signal init_mem              : std_ulogic                                    := '1';
+  signal ctrl_en               : std_ulogic                                    := '0';
+  signal ctrl_we               : std_ulogic                                    := '0';
+  signal ctrl_tag_we           : std_ulogic                                    := '0';
+  signal ctrl_valid_we         : std_ulogic                                    := '0';
+  signal ctrl_invalid_we       : std_ulogic                                    := '0';
+  signal ctrl_addr             : std_ulogic_vector(31 downto 0)                := x"00000000";
+  signal ctrl_wdata            : std_ulogic_vector(31 downto 0)                := x"00000000";
 
   type host_rdata_t is array (0 to DCACHE_NUM_SETS-1) of std_ulogic_vector(31 downto 0);
   
-  signal host_re            : std_ulogic := '0';
-  signal host_addr          : std_ulogic_vector(31 downto 0) := x"00000000";
-  signal host_rdata         : host_rdata_t;
+  signal host_re               : std_ulogic                                    := '0';
+  signal host_addr             : std_ulogic_vector(31 downto 0)                := x"00000000";
+  signal host_rdata            : host_rdata_t;
 
-  signal hit                : std_ulogic_vector(DCACHE_NUM_SETS-1 downto 0) := (others => '0');
+  signal hit                   : std_ulogic_vector(DCACHE_NUM_SETS-1 downto 0) := (others => '0');
 
-  signal timeout_err        : std_ulogic := '0';
-  signal invalid_err        : std_ulogic := '0';
-  signal invalid_block_err  : std_ulogic := '0';
-  signal bad_data_read_err  : std_ulogic := '0';
-  signal data_read_err      : std_ulogic := '0';
-  signal tb_error           : std_ulogic := '0';
-  signal tb_finished        : std_ulogic := '0';
+  signal timeout_err           : std_ulogic                                    := '0';
+  signal invalid_err           : std_ulogic                                    := '0';
+  signal invalid_block_err     : std_ulogic                                    := '0';
+  signal bad_data_read_err     : std_ulogic                                    := '0';
+  signal data_read_err         : std_ulogic                                    := '0';
+  signal tb_error              : std_ulogic                                    := '0';
+  signal tb_finished           : std_ulogic                                    := '0';
   
 begin
 
   -- Clock/Reset Generator ---------------------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
-  clk_gen <= not clk_gen after (t_clock_c/2) when run_tb = '1' else '0';
+  clk_gen <= not clk_gen after (T_CLOCK_C/2) when run_tb = '1' else '0';
 
   -- Testbench error/completion messages -------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
@@ -138,23 +142,14 @@ begin
     end process self_term_proc;
   end generate self_term_gen;
 
-  test_write: process(host_rdata,ctrl_wdata)
-  begin
-    if host_rdata(0) = ctrl_wdata then
-      wrt_success   <= '1';
-    else
-      wrt_success   <= '0';
-    end if;
-  end process test_write;
-
   -- Main control process ----------------------------------------------------------------------
   -- -------------------------------------------------------------------------------------------
   run_test : process
   begin
     if init_mem = '1' then -- Fill cache from memory at beginning of testbench
       if curr_set < DCACHE_NUM_SETS then
-        wait until rising_edge(clk_gen);
-        host_re       <= '0';
+
+        wait until rising_edge(clk_gen); -- Update write address
         ctrl_en       <= '1';
         ctrl_we       <= '1';
         ctrl_tag_we   <= '1';
@@ -162,7 +157,7 @@ begin
         ctrl_addr     <= std_ulogic_vector(curr_addr);
         ctrl_wdata    <= cache_ext_mem(to_integer(curr_addr)); -- From neorv32_dcache_memory_tb_pkg.vhd (run dmem_gen.py to generate)
 
-        wait until rising_edge(clk_gen);
+        wait until rising_edge(clk_gen); -- Enable read
         ctrl_en       <= '0';
         ctrl_we       <= '0';
         ctrl_tag_we   <= '0';
@@ -170,14 +165,22 @@ begin
         host_re       <= '1';
         host_addr     <= std_ulogic_vector(curr_addr);
 
-        wait for 2*t_clock_c;
-        wait until rising_edge(clk_gen);
-          if curr_addr = 4 * (cache_entries_c - 1) then
-            --curr_addr <= (others => '0');
-            curr_set  <= curr_set + 1;
-          else
-            curr_addr <= curr_addr + 4;
-          end if;
+        wait until rising_edge(clk_gen); -- One clock cycle to change hit to true
+        host_re       <= '0';
+        if curr_addr mod (CACHE_ENTRIES_C - 1) = 0 then -- Calculate next address
+          curr_set  <= curr_set  + 1;
+          curr_addr <= curr_addr + 1;
+        else
+          curr_addr <= curr_addr + 1;
+        end if;
+
+        wait until rising_edge(clk_gen); -- Change read address to not match write address
+        host_re       <= '1';
+        host_addr     <= std_ulogic_vector(curr_addr);
+  
+        wait until rising_edge(clk_gen); -- One cycle delay to change hit to false
+        host_re       <= '0';
+
       else
         wait until rising_edge(clk_gen);
         init_mem <= '0';
@@ -195,13 +198,13 @@ begin
       host_addr         <= x"00000000";
       host_re           <= '1';
 
-      wait for 5*t_clock_c;
+      wait for 5*T_CLOCK_C;
       wait until rising_edge(clk_gen);
       ctrl_invalid_we   <= '0';
       invalid_err       <= or_reduce_f(hit);
 
       -- Revalidate block, attempt a read and report error if unsuccessful
-      wait for 5*t_clock_c;
+      wait for 5*T_CLOCK_C;
       wait until rising_edge(clk_gen);
       ctrl_valid_we     <= '1';
       ctrl_en           <= '1';
@@ -210,30 +213,30 @@ begin
       ctrl_en           <= '0';
       host_re           <= '1';
 
-      wait for 5*t_clock_c;
+      wait for 5*T_CLOCK_C;
       wait until rising_edge(clk_gen);
       ctrl_valid_we     <= '0';
       invalid_block_err <= not(and_reduce_f(hit));
-      host_addr         <= std_ulogic_vector(to_unsigned(DCACHE_NUM_BLOCKS*cache_entries_c - 1, 32));
+      host_addr         <= std_ulogic_vector(to_unsigned(DCACHE_NUM_BLOCKS*CACHE_ENTRIES_C - 1, 32));
 
       -- Tests while sets are valid
       -- Read data that is not in cache, report error if successful
-      wait for 5*t_clock_c;
+      wait for 5*T_CLOCK_C;
       wait until rising_edge(clk_gen);
       bad_data_read_err <= or_reduce_f(hit);
       host_re           <= '0';
 
       -- Write then read, report error if unsuccessful
-      wait for 5*t_clock_c;
+      wait for 5*T_CLOCK_C;
       wait until rising_edge(clk_gen);
       ctrl_en           <= '1';
       ctrl_we           <= '1';
       ctrl_tag_we       <= '1';
       ctrl_valid_we     <= '1';
-      ctrl_addr         <= std_ulogic_vector(to_unsigned(DCACHE_NUM_BLOCKS*cache_entries_c - 1, 32));
-      ctrl_wdata        <= cache_ext_mem(DCACHE_NUM_BLOCKS*cache_entries_c - 1); -- From neorv32_dcache_memory_tb_pkg.vhd (run dmem_gen.py to generate)
+      ctrl_addr         <= std_ulogic_vector(to_unsigned(DCACHE_NUM_BLOCKS*CACHE_ENTRIES_C - 1, 32));
+      ctrl_wdata        <= cache_ext_mem(DCACHE_NUM_BLOCKS*CACHE_ENTRIES_C - 1); -- From neorv32_dcache_memory_tb_pkg.vhd (run dmem_gen.py to generate)
 
-      wait for 5*t_clock_c;
+      wait for 5*T_CLOCK_C;
       wait until rising_edge(clk_gen);
       ctrl_en           <= '0';
       ctrl_we           <= '0';
@@ -241,11 +244,11 @@ begin
       ctrl_valid_we     <= '0';
       host_re           <= '1';
 
-      wait for 5*t_clock_c;
+      wait for 5*T_CLOCK_C;
       wait until rising_edge(clk_gen);
       data_read_err     <= not(or_reduce_f(hit));
 
-      wait for 5*t_clock_c;
+      wait for 5*T_CLOCK_C;
       wait until rising_edge(clk_gen);
       host_re           <= '0';
       tb_finished       <= '1';
